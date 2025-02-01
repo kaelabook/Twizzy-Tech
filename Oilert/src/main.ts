047cf218ad28cfad0610cbcf25d7df77bb3ff478
subixsh04 <subikshasvaidhyanathan@gmail.com>
2025-02-01 16:23:22 -0500

    login folder deleted

--- Oilert/src/main.ts
+++ Oilert/src/main.ts
@@ -2,13 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-import { LoginComponent } from './app/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent }, // Show main Oilert page
-  { path: 'login', component: LoginComponent } // Login page (but non-functional)
+  { path: 'app', component: AppComponent } // Main App route
 ];
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()]
-});
