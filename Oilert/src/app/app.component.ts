7e4afa6652522a45caf57a4eb94a8acf3d20432b
subixsh04 <subikshasvaidhyanathan@gmail.com>
2025-02-01 15:02:39 -0500

    added main page

--- Oilert/src/app/app.component.ts
+++ Oilert/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Oilert';
--- Oilert/src/main.ts
+++ Oilert/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { LoginComponent } from './app/login/login.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: '', component: AppComponent }, // Show main Oilert page
+  { path: 'login', component: LoginComponent } // Login page (but non-functional)
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes), provideHttpClient()]
+});
